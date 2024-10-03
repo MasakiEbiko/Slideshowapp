f36afdffe57a7c7be972de062111f7d0db8e9ce0
masakiEbiko <ebiko@kyoei-.com>
2024-10-03 14:29:42 +0900

    Initial Commit

---
+++ Slideshowapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Slideshowapp
+//
+//  Created by mba2408.spacegray kyoei.engine on 2024/10/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
